bf3e97d5c5d9f53e1c68194eae07071ce21ae6ae
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-07-24 09:48:19 -0400

    update references

--- source/elements/drawer.js
+++ source/elements/drawer.js
@@ -1,14 +1,11 @@
-import { Component, template } from '../../references/quantum.js';
 import html from '../templates/drawer.js';
 
-export class Drawer extends Component {
+export class Drawer extends quantum.Component {
     constructor() {
         super();
     }
 
-    static template = template(html);
-
-    static attributes = [];
+    static template = quantum.template(html);
 }
 
-customElements.define('quantum-drawer', Drawer);
+quantum.define('quantum-drawer', Drawer);
